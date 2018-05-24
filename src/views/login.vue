aaad5275798b0a3fb6bf86fcacadaf4edba24f24
EmTao03 <1965058827@qq.com>
2018-05-24 15:54:55 +0800

    remove default value

--- src/views/login.vue
+++ src/views/login.vue
@@ -37,7 +37,7 @@ export default {
     data () {
         return {
             form: {
-                userName: '001',
+                userName: '',
                 password: ''
             },
             rules: {
